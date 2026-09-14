1e0e70b2e20dae5cec34f9348ce2e02d732639d4
keykeivsqz <springtrapk2005@gmail.com>
2026-09-13 22:44:00 -0600

    Final 13 de Septiembre

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 // Por ahora no necesitamos JavaScript, pero lo dejamos listo
 // para cuando querramos agregar interactividad (ej. un formulario de contacto,
 // un modo claro/oscuro, o animaciones al hacer scroll).
-EOF
