eaa810ceff1bfafa7defecd015a8f46b62ef7c12
keykeivsqz <springtrapk2005@gmail.com>
2026-09-13 20:09:41 -0600

    Primer hola mundo

---
+++ script.js
@@ -0,0 +1,6 @@
+const boton = document.getElementById("boton");
+const mensaje = document.getElementById("mensaje");
+
+boton.addEventListener("click", () => {
+  mensaje.textContent = "¡JavaScript está funcionando! 🚀";
+});
